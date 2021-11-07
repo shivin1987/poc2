58794805936159ffb7c553c1953508537d46da93
shivin1987 <shivin1987@gmail.com>
2021-11-07 14:38:46 -0600

    second

--- db-sample-schemas-master/human_resources/hr_main.sql
+++ db-sample-schemas-master/human_resources/hr_main.sql
@@ -53,26 +53,6 @@ rem   ngreenbe  06/01/00 - created
 SET ECHO OFF
 SET VERIFY OFF
 
-PROMPT 
-PROMPT specify password for HR as parameter 1:
-DEFINE pass     = &1
-PROMPT 
-PROMPT specify default tablespeace for HR as parameter 2:
-DEFINE tbs      = &2
-PROMPT 
-PROMPT specify temporary tablespace for HR as parameter 3:
-DEFINE ttbs     = &3
-PROMPT 
-PROMPT specify password for SYS as parameter 4:
-DEFINE pass_sys = &4
-PROMPT 
-PROMPT specify log path as parameter 5:
-DEFINE log_path = &5
-PROMPT
-PROMPT specify connect string as parameter 6:
-DEFINE connect_string     = &6
-PROMPT
-
 -- The first dot in the spool command below is 
 -- the SQL*Plus concatenation character
 
@@ -83,8 +63,6 @@ REM =======================================================
 REM cleanup section
 REM =======================================================
 
-DROP USER hr CASCADE;
-
 REM =======================================================
 REM create user
 REM three separate commands, so the create user command 
@@ -92,65 +70,63 @@ REM will succeed regardless of the existence of the
 REM DEMO and TEMP tablespaces 
 REM =======================================================
 
-CREATE USER hr IDENTIFIED BY &pass;
+CREATE USER hr IDENTIFIED BY OacAdw2021_;
 
-ALTER USER hr DEFAULT TABLESPACE &tbs
-              QUOTA UNLIMITED ON &tbs;
+ALTER USER hr DEFAULT TABLESPACE DATA
+              QUOTA UNLIMITED ON DATA;
 
-ALTER USER hr TEMPORARY TABLESPACE &ttbs;
+ALTER USER hr TEMPORARY TABLESPACE TEMP;
 
 GRANT CREATE SESSION, CREATE VIEW, ALTER SESSION, CREATE SEQUENCE TO hr;
 GRANT CREATE SYNONYM, CREATE DATABASE LINK, RESOURCE , UNLIMITED TABLESPACE TO hr;
-
+GRANT DWROLE TO hr;
 REM =======================================================
 REM grants from sys schema
 REM =======================================================
 
-CONNECT sys/&pass_sys@&connect_string AS SYSDBA;
-GRANT execute ON sys.dbms_stats TO hr;
 
 REM =======================================================
 REM create hr schema objects
 REM =======================================================
 
-CONNECT hr/&pass@&connect_string
-ALTER SESSION SET NLS_LANGUAGE=American;
-ALTER SESSION SET NLS_TERRITORY=America;
+--CONNECT hr/&pass@&connect_string
+--ALTER SESSION SET NLS_LANGUAGE=American;
+--ALTER SESSION SET NLS_TERRITORY=America;
 
 --
 -- create tables, sequences and constraint
 --
 
-@__SUB__CWD__/human_resources/hr_cre
+--@__SUB__CWD__/human_resources/hr_cre
 
 -- 
 -- populate tables
 --
 
-@__SUB__CWD__/human_resources/hr_popul
+--@__SUB__CWD__/human_resources/hr_popul
 
 --
 -- create indexes
 --
 
-@__SUB__CWD__/human_resources/hr_idx
+--@__SUB__CWD__/human_resources/hr_idx
 
 --
 -- create procedural objects
 --
 
-@__SUB__CWD__/human_resources/hr_code
+--@__SUB__CWD__/human_resources/hr_code
 
 --
 -- add comments to tables and columns
 --
 
-@__SUB__CWD__/human_resources/hr_comnt
+--@__SUB__CWD__/human_resources/hr_comnt
 
 --
 -- gather schema statistics
 --
 
-@__SUB__CWD__/human_resources/hr_analz
+--@__SUB__CWD__/human_resources/hr_analz
 
 spool off
